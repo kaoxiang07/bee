f818784f8d6c3505f27e4c2ad32e958193053f7b
root <root@localhost.localdomain>
2021-06-04 08:59:23 +0800

    fix bug

--- bee.sh
+++ bee.sh
@@ -8,6 +8,5 @@ sudo dpkg -i bee-clef_0.4.12_amd64.deb
 #Install and Start bee
 wget https://github.com/ethersphere/bee/releases/download/v0.6.2/bee_0.6.2_amd64.deb
 sudo dpkg -i bee_0.6.2_amd64.deb
-mv password /var/lib/bee-clef/
 	        tmux new -s bee -d -n bee
 		tmux send-keys -t bee:bee "expect startbee.sh" C-m
--- startbee.sh
+++ startbee.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/expect
-password=qiaomu@@1309
+
+set password qiaomu@@1309
 spawn bee start --verbosity 5 --swap-endpoint wss://goerli.infura.io/ws/v3/26dd0a4d0c944e79a2454ab46f982557 --debug-api-enable --clef-signer-enable --clef-signer-endpoint /var/lib/bee-clef/clef.ipc
 expect {
         -re "Password:" {send "${password}\r"
