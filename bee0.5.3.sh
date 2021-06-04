3d55f2a8dde21f139e42766d3db2632c18044640
root <root@localhost.localdomain>
2021-06-04 10:04:34 +0800

    add version

--- bee0.5.3.sh
+++ bee0.5.3.sh
@@ -6,8 +6,8 @@ sudo dpkg -i bee-clef_0.4.12_amd64.deb
 	tmux send-keys -t beeclef:beeclef "bash clef-service.sh start" C-m
 
 #Install and Start bee
-wget https://github.com/ethersphere/bee/releases/download/v0.6.2/bee_0.6.2_amd64.deb
-sudo dpkg -i bee_0.6.2_amd64.deb
+wget https://github.com/ethersphere/bee/releases/download/v0.5.3/bee_0.5.3_amd64.deb
+sudo dpkg -i bee_0.5.3_amd64.deb
 	tmux new -s bee -d -n bee
 	tmux send-keys -t bee:bee "expect startbee.sh" C-m
 string=$(sudo bee-get-addr | grep Ethereum)
