f78b105a0dbcaea494ff1b489084baddae64e20b
kaoxiang07 <kaoxiang07@github.com>
2021-06-11 16:19:58 +0800

    fix

--- restartbee.sh
+++ restartbee.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #Restart bee
-for ((i=0; i<10; i++)); do
+for ((i=2; i<11; i++)); do
 	tmux send-keys -t bee$i:bee$i "expect startbee.sh $i $((1633+$i*10)) $((1635+$i*10)) $((1634+$i*10)) $((6831+$i*10))" C-m
 done
 
