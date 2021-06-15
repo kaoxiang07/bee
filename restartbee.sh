88be747ad3868f77646926ccf9df21b0f8b2260c
kaoxiang07 <kaoxiang07@github.com>
2021-06-15 17:41:32 +0800

    fix bugs

--- restartbee.sh
+++ restartbee.sh
@@ -2,5 +2,5 @@
 #Restart bee
 for ((i=0; i<80; i++)); do
 	tmux send-keys -t bee$i:bee$i "expect startbee.sh $i $((1633+$i*10)) $((1635+$i*10)) $((1634+$i*10)) $((6831+$i*10))" C-m
+	echo Now restarting bee node $i
 done
-
--- startbee.sh
+++ startbee.sh
@@ -6,7 +6,7 @@ set i2 [lindex $argv 2]
 set i3 [lindex $argv 3]
 set i4 [lindex $argv 4]
  
-spawn bee start --verbosity 5 --swap-endpoint https://goerli.infura.io/v3/8977a93858994c8a99cae227e6e03040 --debug-api-enable --clef-signer-enable --clef-signer-endpoint /mnt/.bee-clef$i/clef.ipc --api-addr 127.0.0.1:$i1 --debug-api-addr 127.0.0.1:$i2 --p2p-addr 127.0.0.1:$i3 --tracing-endpoint 127.0.0.1:$i4 --data-dir /mnt/.bee$i --config /mnt/.bee$i/bee.yaml --password $password
+spawn bee start --verbosity 5 --swap-endpoint https://goerli.infura.io/v3/8977a93858994c8a99cae227e6e03040 --debug-api-enable --clef-signer-enable --clef-signer-endpoint /mnt/tmp/.bee-clef$i/clef.ipc --api-addr 127.0.0.1:$i1 --debug-api-addr 127.0.0.1:$i2 --p2p-addr 127.0.0.1:$i3 --tracing-endpoint 127.0.0.1:$i4 --data-dir /mnt/tmp/.bee$i --config /mnt/tmp/.bee$i/bee.yaml --password $password
 
 expect {
         -re "Password:" {send "${password}\r"
